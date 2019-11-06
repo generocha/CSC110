a5751ba2b4cc4b7d2e5c84400d7fefb311505c84
generocha <gene@generocha.com>
2019-11-06 10:48:37 -0800

    remove neg and pos freq var and move score var

--- week6/wordfreq.py
+++ week6/wordfreq.py
@@ -23,6 +23,8 @@ def main():
     counts = {}
     negCounts ={}
     posCounts = {}
+    # add score variable to keep track of sentiment score. Increment for negative words and decrement for positive words.
+    score = 0
     for w in words:
         # check if word is a stop word. If so don't add to counts dictionary
         if w not in stopwords:
@@ -30,13 +32,11 @@ def main():
         # check if word is a negative word. If so add to negCounts dictionary 
         if w in negWords:
             negCounts[w] = negCounts.get(w,0) + 1
+            score -= 1
         # check if word is a positive word. If so add to posCounts dictionary
         if w in posWords:
             posCounts[w] = posCounts.get(w,0) + 1
-    # varibale for negative and positive frequency
-    negFreq = 0
-    posFreq = 0
-    score = 0
+            score += 1
     # output analysis of n most frequent words.
     n = int(input("Output analysis of how many words? "))
     items = list(counts.items())
@@ -45,39 +45,22 @@ def main():
     for i in range(n):
         word, count = items[i]
         print("{0:<15}{1:>5}".format(word, count))
-    # get the total word frequency for negative words
+    # output analysis of 5 most negative frequent words.
     negitems = list(negCounts.items())
     negitems.sort()
     negitems.sort(key=byFreq, reverse=True)
-    for n in range(len(negCounts)):
-        word, count = negitems[n]
-        # increment the negFreq
-        negFreq += count
-        # decrement the score
-        score -= 1
-    # output analysis of 5 most negative frequent words.
     print("The top 5 negative words in these reviews are: ")
     for n in range(5):
         word, count = negitems[n]
         print("{0:<15}{1:>5}".format(word, count))
-    print("The frequency of negative words in these reviews are", negFreq)
-     # get the total word frequency for negative words
+    # output analysis of 5 most positive frequent words.
     positems = list(posCounts.items())
     positems.sort()
     positems.sort(key=byFreq, reverse=True)
-    for p in range(len(posCounts)):
-        word, count = positems[p]
-        # increment the posFreq
-        posFreq += count
-        # increment the score
-        score += 1
-    # output analysis of 5 most positive frequent words.
     print("The top 5 positive words in these reviews are: ")
     for n in range(5):
         word, count = positems[n]
         print("{0:<15}{1:>5}".format(word, count))
-    # print the posFreq
-    print("The frequency of positive words in these reviews are", posFreq)
     # print the sentiment score
     print("The sentiment score for these reviews is",score)
 if __name__ == '__main__':  main()
